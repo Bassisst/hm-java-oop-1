c4b2101563866c8f43eada73f3c08d85aa64fb5f
Bassisst <135557591+Bassisst@users.noreply.github.com>
2024-09-19 14:23:04 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,26 @@
-package sample;
+package oneple;
 
 public class Main {
 
     public static void main(String[] args) {
         
-        Triangle first = new Triangle(9.0, 8.0, 9.0);
-        System.out.println(first.getArea());
+        Product laptop = new Product("Lenovo", 1084, 2.65);  
+        Product phone = new Product("Iphone", 800, 0.4);   
+        Product headphones = new Product();
+        
+    
+        headphones.setName("Airpods");
+        headphones.setPrice(370);
+        headphones.setWeight(0.1);
 
-       
-        Triangle second = new Triangle(5.5, 5.0, 7.0);
-        System.out.println(second.getArea());
+        
+        Product[] list = new Product[] {laptop, phone, headphones};
+
+        
+        for (int k = 0; k < list.length; k++) {
+            System.out.printf("Product name: %-6.6s;  price: %8.2f $;  weight: %4.1f kg%n",
+                              list[k].getName(), list[k].getPrice(), list[k].getWeight());
+            System.out.println(list[k].toString());
+        }
     }
 }
-
-
-
